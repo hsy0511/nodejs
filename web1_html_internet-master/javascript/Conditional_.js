a6dc7080488a8f270dcb4291a4f408bbe789041c
hsy0511 <104752580+hsy0511@users.noreply.github.com>
2023-06-01 14:03:35 +0900

    Add files via upload

---
+++ web1_html_internet-master/javascript/Conditional_.js
@@ -0,0 +1,49 @@
+// 기본구조
+
+// if문
+
+console.log('A');
+
+console.log('B');
+
+if(true){
+    console.log('C1'); 
+}
+
+if(false){
+    console.log('C2');
+}
+
+console.log('D');
+
+// if esle 문
+
+console.log('A');
+
+console.log('B');
+
+if(true){
+    console.log('C1'); 
+}else{
+    console.log('C2');
+}
+
+console.log('D');
+
+// if esle if 문
+
+console.log('A');
+
+console.log('B');
+
+if(true){
+    console.log('C1'); 
+
+}else if(false){
+    console.log('C2');
+
+}else{
+    console.log('C3')
+}
+
+console.log('D');
