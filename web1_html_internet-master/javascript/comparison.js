7f19c77a0d92367e315b0e299ca265055c4be726
hsy0511 <104752580+hsy0511@users.noreply.github.com>
2023-06-01 16:14:22 +0900

    Add files via upload

---
+++ web1_html_internet-master/javascript/comparison.js
@@ -0,0 +1,27 @@
+// 이항 연산자 : 왼쪽 항과 오른쪽 항을 연산함
+console.log(1+1);
+
+console.log(1-1);
+
+console.log(1*1);
+
+console.log(1/1);
+
+// 비교연산자 : 왼쪽 항과 오른쪽 항을 비교해서 맞으면 true, 틀리면 false
+console.log(1==1); // true
+
+console.log(1==2); // false
+
+console.log(1!==2); // true
+
+console.log(1>2); //false
+
+console.log(1>=2); //false
+
+console.log(1<=2); // ture
+
+console.log(1<=2); // ture
+
+console.log(1===2); // false 자료형까지 같아야함
+
+console.log(1===1); // true 자료형까지 같아야함
---
+++ web1_html_internet-master/javascript/consample.js
@@ -0,0 +1,21 @@
+var a = 3;
+var b = 7;
+
+if(a<3){
+    console.log('정답')
+}else if(a==b){
+    console.log('2트 성공')
+}else if(a>b){
+    console.log('3트 성공')
+}else if(a>=b){
+    console.log('4트 성공')
+}else if(a===b){
+    console.log('5트 성공')
+}else if(a!==3){
+    console.log('6트 성공')
+}else if(b!==7){
+    console.log('7트 성공')
+}else{
+    console.log('이걸 못맞추네 에휴')
+}
+
